9b0d2cf78b28f6b7af07c3dbe34ae3483404faf7
Akshay Benny <akshay@pop-os.localdomain>
2022-06-04 20:13:34 +0530

    nextjs and tailwind setup

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Twitter</title>
+        <meta
+          name='description'
+          content='Twitter clone developed by Akshay Benny'
+        />
+        <link
+          rel='icon'
+          href='https://pngimg.com/uploads/twitter/twitter_PNG3.png'
+        />
+      </Head>
+    </div>
+  );
+}
